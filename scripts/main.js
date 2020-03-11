e33fba1fcb3bd705263591156913ff1d3d0a89c6
nickglovdev <nickglovdev@gmail.com>
2020-03-11 10:46:23 -0500

    Working on filtering the crime list

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 import { getCriminals } from "./criminals/criminalsDataProvider.js";
 import { CriminalList } from "./criminals/CriminalList.js";
+import { getConvictions } from "./convictions/ConvictionProvider.js";
+import ConvictionSelect from "./convictions/ConvictionSelect.js";
 
 getCriminals().then(() => CriminalList())
+
+getConvictions().then(() => ConvictionSelect() )
