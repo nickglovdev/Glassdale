2905f609ed4a0562dffa7615fb311411f9fa6f54
nickglovdev <nickglovdev@gmail.com>
2020-03-19 11:48:08 -0500

    Redid many of the files so that they would look like the in classes structure so I can better study it

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,13 +1,18 @@
-export const criminalHtml = (criminalObject) => {
-    return `
-      <section class="criminal__profile">
-        <h3>${criminalObject.name}</h3>
-        <ul class="criminal__list">
-          <li>Age: ${criminalObject.age}</li>
-          <li>Crime: ${criminalObject.conviction}</li>
-          <li>Term Start: ${new Date(criminalObject.incarceration.start).toLocaleDateString('en-US')}</li>
-          <li>Term End: ${new Date(criminalObject.incarceration.end).toLocaleDateString('en-US')}</li>
-        </ul>
-      </section>
-    `
+export const Criminal = (criminalObject) => {
+  return `
+  <div class="criminal">
+      <h4>${criminalObject.name}</h4>
+      <div class="criminal__details">
+          <p>Convicted for ${criminalObject.conviction}</p>
+          <p>Arrested by ${criminalObject.arrestingOfficer}</p>
+          <p>Incarcerated between:
+              ${new Date(criminalObject.incarceration.start).toLocaleDateString()} and
+              ${new Date(criminalObject.incarceration.end).toLocaleDateString()}
+          </p>
+          <p>Age: ${criminalObject.age}</p>
+          <button id="associates--${criminalObject.id}">Show Associates</button>
+      </div>
+  </div>
+  `
 }
+
